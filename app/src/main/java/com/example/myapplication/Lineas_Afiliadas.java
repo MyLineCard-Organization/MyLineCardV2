71a7f9209facf60c8170e0cf4bef70446b2d068d
ShoLee01 <miguelalfaro2017@gmail.com>
2022-09-01 10:18:12 -0500

    refactor: test navigating fragments

--- app/src/main/java/com/example/myapplication/Lineas_Afiliadas.java
+++ app/src/main/java/com/example/myapplication/Lineas_Afiliadas.java
@@ -4,11 +4,14 @@ import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -17,6 +20,8 @@ import android.view.ViewGroup;
  */
 public class Lineas_Afiliadas extends Fragment {
 
+
+    ImageButton btnBack;
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -61,7 +66,31 @@ public class Lineas_Afiliadas extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_lineas__afiliadas, container, false);
-    }
+        View view = inflater.inflate(R.layout.fragment_lineas__afiliadas, container, false);
+        /*btnBack = view.findViewById(R.id.lineas_Afiliadas_btn);
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
+                Intent intent = new Intent( getActivity() ,Billetera_Virtual.class);
+                startActivity(intent);
+            }
+        });*/
+
+        /* Parte 2*/
+
+        btnBack = (ImageButton) view.findViewById(R.id.lineas_Afiliadas_btn);
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Fragment fragment = new Billetera_Virtual();
+                FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.replace(R.id.Lineas_Afiliadas_Container, fragment);
+                fragmentTransaction.addToBackStack(null);
+                fragmentTransaction.commit();
+            }
+        });
+        return view;
+    }
 }
