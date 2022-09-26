a246b87272b5a09d8df2389dce3c3368e4d820d8
ShoLee01 <miguelalfaro2017@gmail.com>
2022-09-26 16:52:59 -0500

    refactor: filter administaror

--- app/src/main/java/com/example/myapplication/Sugerencias_reclamos_admin.java
+++ app/src/main/java/com/example/myapplication/Sugerencias_reclamos_admin.java
@@ -14,6 +14,9 @@ import com.example.myapplication.Adapter.ListViewCommentAdmin;
 import com.example.myapplication.Adapter.ListViewTransport;
 import com.example.myapplication.Models.Comment;
 import com.firebase.ui.firestore.FirestoreRecyclerOptions;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.Query;
 
@@ -23,32 +26,45 @@ public class Sugerencias_reclamos_admin extends Fragment {
     RecyclerView recyclerView;
     ListViewCommentAdmin Adapter;
     FirebaseFirestore firebaseFirestore;
+    private FirebaseAuth auth;
+    private FirebaseFirestore db;
 
     @Override
     public void onStart() {
         super.onStart();
-        Adapter.startListening();
+       // Adapter.startListening();
     }
 
     @Override
     public void onStop() {
         super.onStop();
-        Adapter.stopListening();
+        //Adapter.stopListening();
     }
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+        db = FirebaseFirestore.getInstance();
+        auth = FirebaseAuth.getInstance();
         View view = inflater.inflate(R.layout.fragment_sugerencias_reclamos_admin, container, false);
         firebaseFirestore = FirebaseFirestore.getInstance();
         recyclerView=view.findViewById(R.id.recycler_sugerencias);
         recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
-        Query query = firebaseFirestore.collection("comments");
-        FirestoreRecyclerOptions<Comment> firestoreRecyclerOptions = new FirestoreRecyclerOptions
-                .Builder<Comment>().setQuery(query,Comment.class).build();
-        Adapter = new ListViewCommentAdmin(firestoreRecyclerOptions);
-        Adapter.notifyDataSetChanged();
-        recyclerView.setAdapter(Adapter);
+
+        db.collection("administrator").document(auth.getCurrentUser().getUid())
+                .get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+                    @Override
+                    public void onSuccess(DocumentSnapshot documentSnapshot) {
+                        Query query = firebaseFirestore.collection("comments").whereEqualTo("id_transport", documentSnapshot.getString("transport_id"));
+                        FirestoreRecyclerOptions<Comment> firestoreRecyclerOptions = new FirestoreRecyclerOptions
+                                .Builder<Comment>().setQuery(query,Comment.class).build();
+                        Adapter = new ListViewCommentAdmin(firestoreRecyclerOptions);
+                        Adapter.notifyDataSetChanged();
+                        recyclerView.setAdapter(Adapter);
+                        Adapter.startListening();
+                    }
+                });
+
         return view;
     }
 }
