4564d9ce06ca422c1e42852f2764c7f73ddf9f6c
ShoLee01 <miguelalfaro2017@gmail.com>
2022-09-29 19:04:01 -0500

    refactor: Remodel

--- app/src/main/java/com/example/myapplication/Rutas.java
+++ app/src/main/java/com/example/myapplication/Rutas.java
@@ -7,58 +7,31 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.ViewFlipper;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link Rutas#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class Rutas extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public Rutas() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment Rutas.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static Rutas newInstance(String param1, String param2) {
-        Rutas fragment = new Rutas();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
-
+    ViewFlipper viewFlipper;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_rutas, container, false);
+        View view =inflater.inflate(R.layout.fragment_rutas, container, false);
+        viewFlipper = view.findViewById(R.id.viewFlipper);
+        int images[] = {R.drawable.ruta1,R.drawable.ruta2,R.drawable.ruta3};
+        for (int image:images){
+            flipperImages(image);
+        }
+        return view;
+    }
+
+    public void flipperImages(int image){
+        ImageView imageView = new ImageView(getContext());
+        imageView.setBackgroundResource(image);
+        viewFlipper.addView(imageView);
+        viewFlipper.setFlipInterval(5000);
+        viewFlipper.setAutoStart(true);
+        viewFlipper.setInAnimation(getContext(),android.R.anim.slide_in_left);
+        viewFlipper.setOutAnimation(getContext(),android.R.anim.slide_out_right);
     }
 }
