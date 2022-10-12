917d2ad88fd21b1fced489e0603c334925b9ec9b
Josmarvs <siriusjomsar123@gmail.com>
2022-10-11 21:48:15 -0500

    manual updated

--- app/src/main/java/com/example/myapplication/Manual_Instrucciones.java
+++ app/src/main/java/com/example/myapplication/Manual_Instrucciones.java
@@ -1,8 +1,10 @@
 package com.example.myapplication;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.viewpager2.widget.ViewPager2;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,83 +12,30 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.ViewFlipper;
 
-/*/**
- * A simple {@link Fragment} subclass.
- * Use the {@link Manual_Instrucciones#newInstance} factory method to
- * create an instance of this fragment.
- */
+import com.example.myapplication.Adapter.RutasItemAdapter;
+import com.example.myapplication.Models.RutasItem;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Manual_Instrucciones extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    /*private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public Manual_Instrucciones() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment Manual_Instrucciones.
-     */
-    /*// TODO: Rename and change types and number of parameters
-    public static Manual_Instrucciones newInstance(String param1, String param2) {
-        Manual_Instrucciones fragment = new Manual_Instrucciones();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
-
+    private ViewPager2 viewPager2;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_manual__instrucciones, container, false);
-    }*/
 
-    ViewFlipper viewFlipper;
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view =inflater.inflate(R.layout.fragment_rutas, container, false);
-        viewFlipper = view.findViewById(R.id.viewFlipper);
-        int images[] = {R.drawable.instrucion1,R.drawable.instrucion2};
-        for (int image:images){
-            flipperImages(image);
-        }
+        View view =inflater.inflate(R.layout.fragment_manual__instrucciones, container, false);
+        viewPager2 = view.findViewById(R.id.viewPagerImageManual);
+        List<RutasItem> rutasItems = new ArrayList<>();
+
+        rutasItems.add(new RutasItem(R.drawable.instrucion1));
+        rutasItems.add(new RutasItem(R.drawable.instrucion2));
+
+        viewPager2.setAdapter(new RutasItemAdapter(rutasItems, viewPager2));
         return view;
-    }
 
-    public void flipperImages(int image){
-        ImageView imageView = new ImageView(getContext());
-        imageView.setBackgroundResource(image);
-        viewFlipper.addView(imageView);
-        //viewFlipper.setFlipInterval(3000);
-        viewFlipper.setAutoStart(true);
-        viewFlipper.setInAnimation(getContext(),android.R.anim.slide_in_left);
-        viewFlipper.setOutAnimation(getContext(),android.R.anim.slide_out_right);
-    }
 
+    }
 
 }
